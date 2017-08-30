46cb30d274b0f88797d37cd3b1783c3297e503ae
Gidraff Karanja <gidraffkaranja@Raffs-Pro.local>
2017-08-30 17:51:17 +0300

    Set up fixture for Test cases

---
+++ tests/test_endpoints.py
@@ -0,0 +1,11 @@
+import os
+import unittest
+
+
+class FlaskTest(unittest.TestCase):
+    """Test fof flask routes"""
+
+    def setUp(self):
+        """Test Case fixtures"""
+        pass
+    pass
