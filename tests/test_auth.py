bb1b24822dfcceeba694f294bc1259223f171645
Gidraff Karanja <gidraffkaranja@Raffs-Pro.local>
2017-08-31 18:20:26 +0300

    Fix import error

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,9 +1,23 @@
 import unittest
-from app import app
+from app import create_app, db
+
+
 
 class FlaskTest(unittest.TestCase):
     """Test fof flask routes"""
 
     def setUp(self):
         """Test Case fixtures"""
-        pass
+        self.app = create_app('testing')
+        self.client = self.app.test_client()
+        self.user_data = {
+            'username': 'janedoe',
+            'email': 'janedoe@janedoe.com',
+            'password': 'janedoe@123' 
+        }
+
+        with self.app.app_context():
+            db.session.close()
+            db.drop_all()
+            db.create_all()
+    
