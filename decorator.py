f451e6fcdd68888e22a5765cde19dfed5b564a0d
Gidraff Karanja <gidraffkaranja@Raffs-Pro.local>
2017-09-08 10:08:06 +0300

    Add file for login

---
+++ decorator.py
@@ -0,0 +1,28 @@
+from flask import jsonify, request
+from app.models import User
+
+from functools import wraps
+
+
+def login_required(func):
+
+    @wraps
+    def decorator(*args, **kwargs):
+        auth_token = request.headers.get('Authorization')
+
+        if not auth_token:
+            responseObject = {
+                "message": "Invalid token.Please registe or login",
+            }
+            return jsonify(responseObject), 401
+        if auth_token:
+            kwargs['user_id'] = User.decode_token(auth_token)
+
+            if not isinstance(kwargs['user_id'], int):
+                message = kwargs['user_id']
+                response = {
+                    "message": message
+                }
+                return jsonify(responseObject), 401
+            return func(*args, **kwargs)
+        return decorator
