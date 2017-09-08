a89d24d9a16eb91b14b1b68938b7c92558d043c5
Gidraff Karanja <gidraffkaranja@Raffs-Pro.local>
2017-09-08 10:10:36 +0300

    Include import decorators

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
 from flask_api import FlaskAPI
-from flask_sqlalchemy import SQLAlchemy
+from functools import wraps
+from flask import jsonify
 from instance.config import app_config
+from .models import db
 #initialize sqlalchemy
-db = SQLAlchemy()
 
 def create_app(config_name):
     app = FlaskAPI(__name__, instance_relative_config=True)
@@ -10,5 +11,18 @@ def create_app(config_name):
     app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
-        
-    return app
+
+    #registers authentication blueprint
+    from .views import auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    #registers buckelits blueprint
+    from .views import bucketlist_blueprint
+    app.register_blueprint(bucketlist_blueprint)
+
+    #register activity blueprint
+    from .views import activity_blueprint
+    app.register_blueprint(activity_blueprint)
+
+
+    return app   
