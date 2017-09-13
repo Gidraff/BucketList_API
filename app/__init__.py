ed789226f33072f92cd87f35d6e2d068e0523f77
Gidraff Karanja <gidraffkaranja@Raffs-Pro.local>
2017-09-13 10:14:32 +0300

    (Fix) replace unused imports

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,9 @@
 from flask_api import FlaskAPI
-from functools import wraps
-from flask import jsonify
 from instance.config import app_config
 from .models import db
-#initialize sqlalchemy
+
+# initialize sqlalchemy
+
 
 def create_app(config_name):
     app = FlaskAPI(__name__, instance_relative_config=True)
@@ -12,17 +12,16 @@ def create_app(config_name):
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
-    #registers authentication blueprint
-    from .views import auth_blueprint
-    app.register_blueprint(auth_blueprint)
+    # registers authentication blueprint
+    from .views import users_blueprint
+    app.register_blueprint(users_blueprint)
 
-    #registers buckelits blueprint
+    # registers buckelits blueprint
     from .views import bucketlist_blueprint
     app.register_blueprint(bucketlist_blueprint)
 
-    #register activity blueprint
-    from .views import activity_blueprint
-    app.register_blueprint(activity_blueprint)
+    # register activity blueprint
+    from .views import item_blueprint
+    app.register_blueprint(item_blueprint)
 
-
-    return app   
+    return app
