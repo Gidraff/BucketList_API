03b5e4393418d6d49f87a2b1c1cad0bf7bfb7244
Gidraff Karanja <gidraffkaranja@Raffs-Pro.local>
2017-09-25 06:13:56 +0300

    (Fix) remove unused imports

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -1,15 +1,1 @@
-from flask import Blueprint
-
-# Instantiate Blueprint for users blueprint
-users_blueprint = Blueprint("users", __name__)
-from . import users
-
-
-# Instantiate Blueprint for bucketlist blueprint
-bucketlist_blueprint = Blueprint("bucketlist", __name__)
-from . import bucketlist
-
-
-# Instantiate Blueprint for bucketlist blueprint
-item_blueprint = Blueprint("item", __name__)
-from . import bucketlist_item
+"""This is a init file it."""
