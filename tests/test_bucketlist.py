2243a44b9abbbc93b1ea4d2f4376f37de45076fd
Gidraff Karanja <gidraffkaranja@Raffs-Pro.local>
2017-09-25 06:07:26 +0300

    (Fix) Refactor and solve assertion error

--- tests/test_bucketlist.py
+++ tests/test_bucketlist.py
@@ -1,12 +1,14 @@
+"""Module that contains bucketlis test cases."""
 import unittest
 import json
 from app import db, create_app
 
 
 class BucketlistTestCase(unittest.TestCase):
-    """Class for bucketlist test cases"""
+    """Class for bucketlist test cases."""
 
     def setUp(self):
+        """Set up test fixture."""
         self.app = create_app('testing')
         self.client = self.app.test_client()
         self.user = {
@@ -16,117 +18,134 @@ class BucketlistTestCase(unittest.TestCase):
         }
 
         self.bucketlist = {
-            'title': 'travel',
-            'description': 'sail across atlantic'
+            "title": "travel",
+            "description": "sail across atlantic"
         }
         # binds app to the current_context
         with self.app.app_context():
+            db.session.close()
             # creates all tables
             db.create_all()
 
     def registration(self):
-        """Register a user"""
-        response = self.client.post(
+        """Register a user."""
+        self.client.post(
             '/auth/register/',
-            data=self.user,
+            data=json.dumps(self.user),
             content_type='application/json'
         )
 
     def login(self):
-        """logins user"""
-        return self.client.post(
-            '/auth/login',
-            data=self.user,
+        """Login user."""
+        response = self.client.post(
+            '/auth/login/',
+            data=json.dumps(self.user),
             content_type='application/json'
         )
+        return response
 
     def test_bucketlist_creation(self):
-        """Test if bucketlist has been created"""
+        """Test bucketlist creation."""
         self.registration()
-        self.login()
+        token = json.loads(self.login().data)
         res = self.client.post('/bucketlists/',
-                               data=self.bucketlist,
-                               content_type='application/json')
-        self.assertEqual(201, res.status_code)
+                               data=json.dumps(self.bucketlist),
+                               content_type='application/json',
+                               headers={
+                                   "Authorization": token['access_token']})
+        self.assertEqual(res.status_code, 201)
 
     def test_api_can_get_all_bucketlist(self):
-        """
-        Test if an API can return all bucketlists
-        """
+        """Test get all bucketlist."""
         self.registration()
-        self.login()
-
+        token = json.loads(self.login().data)
         res = self.client.post(
             '/bucketlists/',
-            data=self.bucketlist,
-            content_type='application/json')
-        self.assertEqual(201, res.status_code)
-        rev = self.client.get('/bucketlists/')
+            data=json.dumps(self.bucketlist),
+            content_type='application/json',
+            headers={'Authorization': token["access_token"]})
+        rev = self.client.get(
+            '/bucketlists/',
+            content_type='application/json',
+            headers={'Authorization': token["access_token"]})
         self.assertEqual(200, rev.status_code)
 
     def test_bucketlist_can_be_edited(self):
-        """Test if bucketlis can be updated"""
+        """Test bucketlist editing."""
         self.registration()
-        self.login()
-
-        res = self.client.post(
-            '/bucketlists/', data={
-                'title': 'Go to',
-                'description': 'Nort pole for weekened'
-            }, content_type='application/json')
-        self.assertEqual(201, res.status_code)
-        rev = self.client.put(
-            '/bucketlists/1',
-            data={
-                'title': 'Go to hawaii',
-                'descrption': 'go for vacation'
-            }, content_type='application/json'
-        )
-        self.assertEqual(200, rev.status_code)
-
-    def test_api_can_get_bucketlist_by_id(self):
-        """Test if bucketlist can get a specific
-            bucketlist using it's id
-        """
-        self.registration()
-        self.login()
+        token = json.loads(self.login().data)
 
         res = self.client.post(
             '/bucketlists/',
-            data=self.bucketlist,
-            content_type='application/json'
+            data=json.dumps({
+                'title': 'Goto',
+                'description': 'Nort pole for weekened'}),
+            content_type='application/json',
+            headers={"Authorization": token["access_token"]})
+
+        results = json.loads(res.data.decode())
+        print('results', results)
+
+        rev = self.client.put(
+            '/bucketlists/{}/'.format(results['id']),
+            data=json.dumps({
+                'title': 'Go to hawaii',
+                'description': 'goforvacation'}),
+            content_type='application/json',
+            headers={"Authorization": token["access_token"]})
+
+        print("Reeeees", rev.data.decode())
+        self.assertEqual(200, rev.status_code)
+
+    def test_api_can_get_bucketlist_by_id(self):
+        """Test get bucket by id."""
+        self.registration()
+        token = json.loads(self.login().data)
+
+        res = self.client.post(
+            '/bucketlists/',
+            data=json.dumps(self.bucketlist),
+            content_type='application/json',
+            headers={'Authorization': token["access_token"]}
         )
         self.assertEqual(201, res.status_code)
         result_in_json = json.loads(
-            res.data.decode('utf-8'))
+            res.data)
 
         rev = self.client.get(
-            '/bucketlists/{}'.format(
-                result_in_json['id']))
+            '/bucketlists/{}/'.format(
+                result_in_json['id']),
+            content_type='application/json',
+            headers={'Authorization': token["access_token"]})
         self.assertEqual(200, rev.status_code)
-        self.assertIn('travel', rev.data)
+        self.assertIn('travel', str(rev.data))
 
     def test_api_can_delete_bucketlist(self):
-        """Test if a bucketlist can be deleted"""
+        """Test bucketlist deletion."""
         self.registration()
-        self.login()
-
+        response = self.login()
+        data = json.loads(response.data.decode())
         result = self.client.post(
-            '/bucketlists/', data={
+            '/bucketlists/',
+            data=json.dumps({
                 'title': 'travel',
                 'description': 'travel asia'
-            })
-        self.assertEqual(201, result.status_code)
+            }),
+            headers={
+                "Authorization": data['access_token']
+            },
+            content_type='application/json'
+        )
 
-        response = self.client.delete('/bucketlists/1')
+        response = self.client.delete(
+            '/bucketlists/1/',
+            headers={
+                "Authorization": data['access_token']},
+            content_type='application/json')
         self.assertEqual(200, response.status_code)
 
-        rev = self.client.get('/bucketlists/1')
-        self.assertEqual(404, rev.status_code)
-    # delete
-
     def tearDown(self):
-        """Tear down all initialized variables"""
+        """Tear down setup."""
         with self.app.app_context():
             db.session.close()
             db.drop_all()
